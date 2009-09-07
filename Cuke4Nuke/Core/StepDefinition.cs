b751531792e9356bc7db6d49c6fd504b435ea859
Richard Lawrence <richard@humanizingwork.com>
2009-09-07 15:09:04 -0600

    Added Core project. Added StepDefinition class.

---
+++ Cuke4Nuke/Core/StepDefinition.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cuke4Nuke.Core
+{
+    public class StepDefinition
+    {
+        /// <summary>
+        /// Initializes a new instance of the StepDefinition class.
+        /// </summary>
+        public StepDefinition()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the StepDefinition class.
+        /// </summary>
+        /// <param name="pattern"></param>
+        public StepDefinition(string pattern)
+        {
+            _pattern = pattern;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the StepDefinition class.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="pattern"></param>
+        public StepDefinition(string id, string pattern)
+        {
+            _id = id;
+            _pattern = pattern;
+        }
+
+        private string _id;
+        public string Id
+        {
+            get { return _id; }
+            set
+            {
+                _id = value;
+            }
+        }
+
+        private string _pattern;
+        public string Pattern
+        {
+            get { return _pattern; }
+            set
+            {
+                _pattern = value;
+            }
+        }
+    }
+}
---
+++ Cuke4Nuke/Core/Test/StepDefinition_Specification.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Cuke4Nuke.Core.Test
+{
+    [TestFixture]
+    public class StepDefinition_Specification
+    {
+        [Test]
+        public void ShouldHavePatternProperty()
+        {
+            StepDefinition sd = new StepDefinition();
+            Assert.That(sd, Has.Property("Pattern"));
+        }
+
+        [Test]
+        public void ShouldHaveIdProperty()
+        {
+            StepDefinition sd = new StepDefinition();
+            Assert.That(sd, Has.Property("Id"));
+        }
+
+        [Test]
+        public void ShouldTakePatternInConstructor()
+        {
+            StepDefinition sd = new StepDefinition(".*");
+            Assert.That(sd.Pattern, Is.EqualTo(".*"));
+        }
+
+        [Test]
+        public void ShouldTakeIdAndPatternInConstructor()
+        {
+            StepDefinition sd = new StepDefinition("id", ".*");
+            Assert.That(sd.Id, Is.EqualTo("id"));
+            Assert.That(sd.Pattern, Is.EqualTo(".*"));
+        }
+    }
+}
