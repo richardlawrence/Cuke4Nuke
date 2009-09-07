ba96946f01cbf42d3519ed8f1b11700575b350fc
Richard Lawrence <richard@humanizingwork.com>
2009-09-07 16:22:26 -0600

    StepDefinitionRepository can hold StepDefinition objects and return JSON about them as Cucumber's wire protocol requires.

---
+++ Cuke4Nuke/Core/StepDefinitionRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LitJson;
+
+namespace Cuke4Nuke.Core
+{
+    public class StepDefinitionRepository
+    {
+        List<StepDefinition> _stepDefinitions = new List<StepDefinition>();
+
+        public void AddStepDefinition(StepDefinition stepDefinition)
+        {
+            _stepDefinitions.Add(stepDefinition);
+        }
+
+        public List<StepDefinition> StepDefinitions
+        {
+            get
+            {
+                return _stepDefinitions;
+            }
+        }
+
+        public string ListStepDefinitionsAsJson()
+        {
+            StringBuilder sb = new StringBuilder();
+            JsonWriter writer = new JsonWriter(sb);
+            writer.WriteArrayStart();
+            foreach (StepDefinition sd in _stepDefinitions)
+            {
+                writer.WriteObjectStart();
+                writer.WritePropertyName("id");
+                writer.Write(sd.Id);
+                writer.WritePropertyName("regexp");
+                writer.Write(sd.Pattern);
+                writer.WriteObjectEnd();
+            }
+            writer.WriteArrayEnd();
+            return sb.ToString();
+        }
+    }
+}
---
+++ Cuke4Nuke/Core/Test/StepDefinitionRepository_Specification.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.Reflection;
+using LitJson;
+
+namespace Cuke4Nuke.Core.Test
+{
+    [TestFixture]
+    public class StepDefinitionRepository_Specification
+    {
+        [Test]
+        public void ShouldHoldStepDefinitions()
+        {
+            StepDefinitionRepository sdr = new StepDefinitionRepository();
+            StepDefinition sd = CreateStepDefinition();
+            sdr.AddStepDefinition(sd);
+            Assert.That(sdr.StepDefinitions, Has.Member(sd));
+        }
+
+        [Test]
+        public void ShouldListStepDefinitionsAsJson()
+        {
+            StepDefinitionRepository sdr = new StepDefinitionRepository();
+            StepDefinition sd = CreateStepDefinition();
+            sdr.AddStepDefinition(sd);
+            JsonData data = JsonMapper.ToObject(sdr.ListStepDefinitionsAsJson());
+
+            Assert.That(data.IsArray);
+            Assert.That(data.Count, Is.EqualTo(1));
+            Assert.That((string) data[0]["id"], Is.EqualTo(sd.Id));
+            Assert.That((string) data[0]["regexp"], Is.EqualTo(sd.Pattern));
+        }
+
+        private StepDefinition CreateStepDefinition()
+        {
+            MethodInfo dummyMethod = this.GetType().GetMethod("DummyMethod");
+            return new StepDefinition(".*", dummyMethod);
+        }
+
+        public void DummyMethod() { }
+    }
+}
