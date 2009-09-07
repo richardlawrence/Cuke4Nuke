30f2090c4777d1788addf6a4cb29286bfe36ba32
Richard Lawrence <richard@humanizingwork.com>
2009-09-07 10:06:11 -0600

    Simple server and functional test.

---
+++ Cuke4Nuke/Server/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Threading;
+using System.Net;
+
+namespace Cuke4Nuke.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient client;
+            int port = 3901;
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, port);
+            TcpListener listener = new TcpListener(endPoint);
+            listener.Start(0);
+            Console.WriteLine("Listening on port " + port.ToString());
+            while (true)
+            {
+                if (listener.Pending())
+                {
+                    client = listener.AcceptTcpClient();
+                    break;
+                }
+                else
+                {
+                    Thread.Sleep(500);
+                }
+            }
+            Console.WriteLine("Press Enter to exit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Cuke4Nuke/Test/Server_Specification.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.Diagnostics;
+using System.Net.Sockets;
+
+namespace Test
+{
+    [TestFixture]
+    public class Server_Specification
+    {
+        Process serverProcess;
+
+        [SetUp]
+        public void Setup()
+        {
+            string serverExePath = @"..\..\..\Server\bin\Debug\Cuke4Nuke.Server.exe";
+            serverProcess = Process.Start(serverExePath);
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            serverProcess.CloseMainWindow();
+        }
+
+        [Test]
+        public void ShouldAnswerOverTcp()
+        {
+            int port = 3901;
+            TcpClient client = new TcpClient("localhost", port);
+            Assert.That(client.Connected);
+        }
+    }
+}
