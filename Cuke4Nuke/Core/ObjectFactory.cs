4cdfab3df6342a0598d6eae08704d4772d17ebf6
Richard Lawrence <richard@humanizingwork.com>
2009-10-21 20:13:47 -0600

    Added ObjectFactory.

---
+++ Cuke4Nuke/Core/ObjectFactory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cuke4Nuke.Core
+{
+    public class ObjectFactory
+    {
+        List<Type> _classes = new List<Type>();
+        Dictionary<Type, object> _objects = new Dictionary<Type, object>();
+
+        public void AddClass(Type type)
+        {
+            if (!_classes.Contains(type))
+            {
+                _classes.Add(type);
+            }
+        }
+
+        public void CreateObjects()
+        {
+            foreach (Type type in _classes)
+            {
+                _objects.Add(type, Activator.CreateInstance(type));
+            }
+        }
+
+        public object GetObject(Type type)
+        {
+            if (!_objects.ContainsKey(type))
+            {
+                return null;
+            }
+
+            return _objects[type];
+        }
+    }
+}
---
+++ Cuke4Nuke/Specifications/Core/ObjectFactory_Specification.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Cuke4Nuke.Core;
+
+namespace Cuke4Nuke.Specifications.Core
+{
+    public class ObjectFactory_Specification
+    {
+        [Test]
+        public void Should_have_AddClass_method()
+        {
+            ObjectFactory objectFactory = new ObjectFactory();
+            objectFactory.AddClass(typeof(Dummy));
+        }
+
+        [Test]
+        public void Should_have_CreateObjects_method()
+        {
+            ObjectFactory objectFactory = new ObjectFactory();
+            objectFactory.AddClass(typeof(Dummy));
+            objectFactory.CreateObjects();
+        }
+        
+        [Test]
+        public void Should_have_GetObject_method()
+        {
+            ObjectFactory objectFactory = new ObjectFactory();
+            objectFactory.AddClass(typeof(Dummy));
+            objectFactory.CreateObjects();
+            Dummy dummy = (Dummy) objectFactory.GetObject(typeof(Dummy));
+        }
+
+        [Test]
+        public void Should_return_null_from_GetObject_if_CreateObjects_not_called_first()
+        {
+            ObjectFactory objectFactory = new ObjectFactory();
+            objectFactory.AddClass(typeof(Dummy));
+            Assert.That(objectFactory.GetObject(typeof(Dummy)), Is.Null);
+        }
+
+        [Test]
+        public void Should_return_same_object_from_multiple_GetObject_calls()
+        {
+            ObjectFactory objectFactory = new ObjectFactory();
+            objectFactory.AddClass(typeof(Dummy));
+            objectFactory.CreateObjects();
+            Dummy dummy1 = (Dummy)objectFactory.GetObject(typeof(Dummy));
+            dummy1.Value = "foo";
+            Dummy dummy2 = (Dummy)objectFactory.GetObject(typeof(Dummy));
+            Assert.That(dummy2.Value, Is.EqualTo("foo"));
+        }
+    }
+
+    public class Dummy
+    {
+        public String Value { get; set; }
+    }
+}
