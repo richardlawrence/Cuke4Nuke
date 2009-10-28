dfe0c2dbe70f94282af4fb77277519e4faa3bc00
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2009-10-28 02:42:17 +0100

    Added IronRuby example

---
+++ examples/Calc/CalcFeatures/features/step_definitons/calculator_steps.rb
@@ -0,0 +1,23 @@
+require 'spec/expectations'
+$:.unshift(File.dirname(__FILE__) + '/../../bin/Release') # So we find the .dll
+require 'Calc.dll'
+
+Before do
+  @calc = Calc::Calculator.new
+end
+
+Given "I have entered $n into the calculator" do |n|
+  @calc.Push(n.to_f)
+end
+
+When /I press divide/ do
+  @result = @calc.Divide
+end
+
+When /I press add/ do
+  @result = @calc.Add
+end
+
+Then /the result should be (.*) on the screen/ do |result|
+  @result.should == result.to_f
+end
