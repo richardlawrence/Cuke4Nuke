8e4ba7e440bdc43f17805f84d8afb1a216de5af2
Richard Lawrence <richard@humanizingwork.com>
2009-11-25 10:36:30 -0700

    First table scenario and just enough of the Table class to make the code in the scenario compile.

---
+++ Cuke4Nuke/Framework/Table.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cuke4Nuke.Framework
+{
+    public class Table
+    {
+        public IList<Dictionary<string, string>> Hashes
+        {
+            get
+            {
+                return new List<Dictionary<string, string>>();
+            }
+        }
+    }
+}
