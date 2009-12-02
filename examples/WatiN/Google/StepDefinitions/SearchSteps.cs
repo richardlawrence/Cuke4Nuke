a9825045a01c2fbcf5fc3ae353940b3122fd39ba
Richard Lawrence <richard@humanizingwork.com>
2009-12-02 11:34:10 -0700

    Added WatiN example.

---
+++ examples/WatiN/Google/StepDefinitions/SearchSteps.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Cuke4Nuke.Framework;
+using NUnit.Framework;
+using WatiN.Core;
+
+namespace Google.StepDefinitions
+{
+    public class SearchSteps
+    {
+        Browser _browser;
+
+        [Before]
+        public void SetUp()
+        {
+            _browser = new WatiN.Core.IE();
+        }
+
+        [After]
+        public void TearDown()
+        {
+            if (_browser != null)
+            {
+                _browser.Dispose();
+            }
+        }
+
+        [When(@"^(?:I'm on|I go to) the search page$")]
+        public void GoToSearchPage()
+        {
+            _browser.GoTo("http://www.google.com/");
+        }
+
+        [When("^I search for \"(.*)\"$")]
+        public void SearchFor(string query)
+        {
+            _browser.TextField(Find.ByName("q")).TypeText(query);
+            _browser.Button(Find.ByName("btnG")).Click();
+        }
+
+        [Then("^I should be on the search page$")]
+        public void IsOnSearchPage()
+        {
+            Assert.That(_browser.Title == "Google");
+        }
+
+        [Then("^I should see \"(.*)\" in the results$")]
+        public void ResultsContain(string expectedResult)
+        {
+            Assert.That(_browser.ContainsText(expectedResult));
+        }
+    }
+}
---
+++ examples/WatiN/Google/features/search.feature
@@ -0,0 +1,13 @@
+﻿Feature: Google search
+    In order find things on the internet
+    As a user
+    I want to search for web pages with particular text in them
+    
+    Scenario: Load search page
+        When I go to the search page
+        Then I should be on the search page
+
+    Scenario: Search
+        Given I'm on the search page
+        When I search for "richard lawrence"
+        Then I should see "www.richardlawrence.info" in the results
